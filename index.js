c1f2d1fae1c99bb774d8a976d2ffcd9311fb7ef8
rem137 <56608633+rem137@users.noreply.github.com>
2020-08-05 22:36:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('message', msg => {
   if (msg.content.includes("initial")) {
-    msg.author.send("ok");
+    msg.channel.send('message 1').then((message)=> {
+      setTimeout(fonction(){
+                 message.edit('message 2');
+    }, 1000)
+  });
   }
 });
 client.login(process.env.TOKEN);
