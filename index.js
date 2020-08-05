59a6975e52672ecd033d7f06de46650f4a0b0e5e
rem137 <56608633+rem137@users.noreply.github.com>
2020-08-05 22:27:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+client.on('message', msg => {
   if (msg.content.includes("initial")) {
     msg.author.send("ok");
   }
